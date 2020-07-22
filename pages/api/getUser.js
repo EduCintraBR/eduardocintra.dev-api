831dabe8e39e034586ac8bad2ab141b629b4cde4
Eduardo Cintra <educin15@hotmail.com>
2020-07-22 14:42:40 -0300

    adding cache control

--- pages/api/getUser.js
+++ pages/api/getUser.js
@@ -1,7 +1,16 @@
 import getUser from '../../utils/getUser'
+import Cors from 'cors'
+
+const cors = initMiddleware(
+    Cors({
+        methods: ['GET', 'OPTIONS']
+    })
+)
 
 const apiGetUser = async (req, res) => {
     const username = 'educintrabr'
+    await cors(req, res)
+    res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate')
     const data = await getUser(username)
     res.send(data)
 }
